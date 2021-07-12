cc8d5da80bd63f0954ce4d343fd54c5a8074b5ec
Shahmir Masood <shahmirnation@gmail.com>
2021-07-12 17:50:01 -0500

    Update post-build.sh

--- post-build.sh
+++ post-build.sh
@@ -1,1 +1,5 @@
-
+APK_PATH = $APPCENTER_OUTPUT_DIRECTORY/yourapk.apk
+SUBSCRIPTION_KEY = "your subscription key"
+curl --location --request POST 'https://api-sofy-test.azurewebsites.net/api/AppTests/buildUpload' \
+--header 'SubscriptionKey: ${SUBSCRIPTION_KEY}' \
+--form 'applicationFile=@"${APK_PATH}"'
